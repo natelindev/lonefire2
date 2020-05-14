cd50396047e3610b5c7273df090837bd2118637b
Nathaniel <linlilulll@gmail.com>
2020-05-14 09:36:10 +0800

    update typescript & fix typo

--- Frontend/src/components/Masonry.tsx
+++ Frontend/src/components/Masonry.tsx
@@ -2,7 +2,7 @@ import React, { useRef } from 'react';
 import Bricks from 'bricks.js';
 
 export default function Masonry() {
-  const masonryContainter = useRef(null);
+  const masonryContainer = useRef(null);
 
-  return <div ref={masonryContainter}></div>;
+  return <div ref={masonryContainer}></div>;
 }
