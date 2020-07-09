6d103a368840483fa34043e07cbf530e8c57da3a
Nathaniel <linlilulll@gmail.com>
2020-07-10 03:47:12 +0800

    masonry adaptation

--- Frontend/src/view/Home.tsx
+++ Frontend/src/view/Home.tsx
@@ -3,11 +3,13 @@ import React from 'react';
 import { css } from '@emotion/core';
 
 import Card from '../components/Card';
+import Masonry from '../components/Masonry';
+import { rng, RngOption } from '../util/randomGenerator';
 
 const Home = (): React.ReactElement => (
   <div>
     <Card
-      className="border-draw-within mx-auto my-2"
+      className="border-draw-within mx-auto my-3"
       width="20rem"
       css={css`
         font-family: 'Economica', sans-serif;
@@ -17,6 +19,37 @@ const Home = (): React.ReactElement => (
     >
       <h1 className="mx-auto my-4">Nathaniel&#39;s Dev Area</h1>
     </Card>
+    <div className="d-flex justify-content-center mt-5 mb-3">
+      <Masonry
+        packed="packed"
+        sizes={[
+          { columns: 1, gutter: 20 },
+          { mq: '768px', columns: 2, gutter: 20 },
+          { mq: '1024px', columns: 3, gutter: 20 },
+        ]}
+        position
+        css={css`
+          width: 100%;
+          align-self: center;
+          margin: 0 0;
+        `}
+      >
+        {rng(RngOption.array, 400, 200, 20).map((height, index) => (
+          <div
+            key={height}
+            css={css`
+              width: 20rem;
+              height: ${height}px;
+              border: 1px solid black;
+              padding: 4px 4px;
+              margin: auto auto;
+            `}
+          >
+            Article {index + 1}
+          </div>
+        ))}
+      </Masonry>
+    </div>
   </div>
 );
 
